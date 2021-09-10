cef66948f70f3fd4be903d527ee234fbde49886c
Mateus Castro <mateus.castro.domingos@gmail.com>
2021-09-10 11:06:18 -0300

    Configure Tailwind, AntD, Storybook and Eslint/Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DatePicker } from "antd";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello</h1>
+      <img
+        alt="img"
+        className="rounded-full"
+        src="https://store-images.s-microsoft.com/image/apps.15123.14043593014419749.25e534d6-f45f-46a9-980f-d1507690fcbb.026aa59b-fd1f-472d-8483-e40f453eb773?mode=scale&q=90&h=300&w=300"
+      />
+      <DatePicker />
+    </>
   );
-}
+};
 
 export default App;
