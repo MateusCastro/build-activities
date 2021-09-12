eec622f1ce104d3c6896086095f23052ce5a1dd8
Mateus Castro <mateus.castro.domingos@gmail.com>
2021-09-12 20:49:23 -0300

    Finish TimeLineItem

--- src/components/ActivitiesGroup/TimeLineItem.tsx
+++ src/components/ActivitiesGroup/TimeLineItem.tsx
@@ -1,28 +1,143 @@
 import React from "react";
 
 import { Timeline } from "antd";
+import moment from "moment";
 
 import CloudUp from "../assets/img/cloud-up.svg";
+import Hammer from "../assets/img/hammer.svg";
+import CI from "../assets/img/ci.svg";
 
-const TimeLineItem: React.FC = () => (
-  <Timeline.Item
-    dot={
-      <div className="border border-gray-200 rounded-full p-0.5">
-        <img src={CloudUp} alt="cloud-up" className="w-6" />
-        <div
-          className="w-2.5 h-2.5 rounded-full absolute top-1 right-0 border-2 border-white"
-          style={{ backgroundColor: "#00AE55" }}
-        />
-      </div>
+type Item = {
+  createdAt: number;
+  metadata: {
+    user: {
+      userEmail: string;
+      initials: string;
+      name: string;
+      photoUrl: string;
+    };
+  };
+  buildId: string;
+  projectId: string;
+  type: string;
+  services?: {
+    imageHint: string;
+    serviceId: string;
+  }[];
+};
+
+interface Props {
+  item: Item;
+  getProcessingTime: (type: string) => number | undefined;
+}
+
+interface Info {
+  icon: string;
+  statusColor?: string;
+  title: string;
+  timeFromNow: string;
+  processingTime?: number;
+}
+
+const TimeLineItem: React.FC<Props> = ({ item, getProcessingTime }) => {
+  const [service, status] = item.type.split("_");
+
+  const getIcon = () => {
+    switch (service) {
+      case "CI":
+        return CI;
+      case "BUILD":
+        return Hammer;
+      case "DEPLOY":
+        return CloudUp;
+      default:
+        return CloudUp;
     }
-  >
-    <div className="flex flex-col text-sm">
-      <span>
-        Deployment succeeded in <span className="text-gray-500">15s</span>
-      </span>
-      <span className="text-gray-500">2 hours ago</span>
-    </div>
-  </Timeline.Item>
-);
+  };
+
+  const getStatusColor = () => {
+    switch (status) {
+      case "SUCCEEDED":
+        return "bg-green-500";
+      case "FAILED":
+        return "bg-red-500";
+      case "PROGRESS":
+        return "bg-blue-500";
+      case "PARTIALLY":
+        return "bg-yellow-500";
+      case "CANCELED":
+        return "bg-gray-500";
+      default:
+        return "hidden";
+    }
+  };
+
+  const getTitle = () => {
+    const textService = service === "CI" ? "Continuous integration" : service;
+    return `${textService} ${status}`;
+  };
+
+  const capitalize = (text: string) => {
+    const textLowerCase = text.toLowerCase();
+
+    return (
+      textLowerCase.toLowerCase().charAt(0).toUpperCase() +
+      textLowerCase.slice(1)
+    );
+  };
+
+  const loadInfos = () => {
+    const informations: Info = {} as Info;
+    informations.icon = getIcon();
+    informations.statusColor = getStatusColor();
+    informations.title = capitalize(getTitle());
+    informations.processingTime =
+      status !== "STARTED"
+        ? getProcessingTime(`${service}_STARTED`)
+        : undefined;
+
+    return informations;
+  };
+
+  const info = loadInfos();
+
+  const getTime = () => {
+    const time = moment.duration(
+      moment(item.createdAt).diff(info.processingTime)
+    );
+    const timeHumanize = time.humanize();
+
+    return timeHumanize === "a few seconds"
+      ? `${time.asSeconds().toFixed()} seconds`
+      : timeHumanize;
+  };
+
+  return (
+    <Timeline.Item
+      dot={
+        <div className="border border-gray-200 rounded-full p-0.5">
+          <img src={info.icon} alt="service" className="w-6" />
+          <div
+            className={`w-2.5 h-2.5 rounded-full absolute top-1 right-0 border-2 border-white ${info.statusColor}`}
+          />
+        </div>
+      }
+    >
+      <div className="flex flex-col text-sm">
+        <span>
+          {info.title}{" "}
+          {info.processingTime && (
+            <span>
+              in <span className="text-gray-500">{getTime()}</span>
+            </span>
+          )}
+        </span>
+        <span className="text-gray-500">
+          {capitalize(moment(item.createdAt).fromNow())}
+        </span>
+      </div>
+    </Timeline.Item>
+  );
+};
 
 export default TimeLineItem;
--- src/components/ActivitiesGroup/index.tsx
+++ src/components/ActivitiesGroup/index.tsx
@@ -4,7 +4,191 @@ import { Card, Timeline } from "antd";
 import TimeLineHeader from "./TimeLineHeader";
 import TimeLineItem from "./TimeLineItem";
 
+const build = [
+  {
+    createdAt: 1598375525969,
+    metadata: {
+      user: {
+        userEmail: "zeno.rocha@liferay.com",
+        initials: "Z",
+        name: "Zeno Rocha",
+        photoUrl:
+          "https://gravatar.com/avatar/7df4c367ea3a5475f05db9e2d32d5b13?d=blank&s=70",
+      },
+    },
+    buildId: "5",
+    projectId: "acme",
+    type: "CI_SUCCEEDED",
+  },
+  {
+    createdAt: 1598375520341,
+    metadata: {
+      user: {
+        userEmail: "zeno.rocha@liferay.com",
+        initials: "Z",
+        name: "Zeno Rocha",
+        photoUrl:
+          "https://gravatar.com/avatar/7df4c367ea3a5475f05db9e2d32d5b13?d=blank&s=70",
+      },
+    },
+    buildId: "5",
+    projectId: "acme",
+    type: "DEPLOY_SUCCEEDED",
+    services: [
+      {
+        imageHint: "liferaycloud/liferay-dxp",
+        serviceId: "liferay",
+      },
+      {
+        imageHint: "liferaycloud/nginx",
+        serviceId: "webserver",
+      },
+      {
+        imageHint: "liferaycloud/backup",
+        serviceId: "backup",
+      },
+      {
+        imageHint: "liferaycloud/elasticsearch",
+        serviceId: "search",
+      },
+      {
+        imageHint: "liferaycloud/database",
+        serviceId: "database",
+      },
+    ],
+  },
+  {
+    createdAt: 1598375404882,
+    metadata: {
+      user: {
+        userEmail: "zeno.rocha@liferay.com",
+        initials: "Z",
+        name: "Zeno Rocha",
+        photoUrl:
+          "https://gravatar.com/avatar/7df4c367ea3a5475f05db9e2d32d5b13?d=blank&s=70",
+      },
+    },
+    buildId: "5",
+    projectId: "acme",
+    type: "DEPLOY_STARTED",
+    services: [
+      {
+        imageHint: "liferaycloud/backup",
+        serviceId: "backup",
+      },
+      {
+        imageHint: "liferaycloud/nginx",
+        serviceId: "webserver",
+      },
+      {
+        imageHint: "liferaycloud/liferay-dxp",
+        serviceId: "liferay",
+      },
+      {
+        imageHint: "liferaycloud/elasticsearch",
+        serviceId: "search",
+      },
+      {
+        imageHint: "liferaycloud/database",
+        serviceId: "database",
+      },
+    ],
+  },
+  {
+    createdAt: 1598375388777,
+    metadata: {
+      user: {
+        userEmail: "zeno.rocha@liferay.com",
+        initials: "Z",
+        name: "Zeno Rocha",
+        photoUrl:
+          "https://gravatar.com/avatar/7df4c367ea3a5475f05db9e2d32d5b13?d=blank&s=70",
+      },
+    },
+    buildId: "5",
+    projectId: "acme",
+    type: "BUILD_SUCCEEDED",
+    services: [
+      {
+        imageHint: "liferaycloud/backup",
+        serviceId: "backup",
+      },
+      {
+        imageHint: "liferaycloud/nginx",
+        serviceId: "webserver",
+      },
+      {
+        imageHint: "liferaycloud/liferay-dxp",
+        serviceId: "liferay",
+      },
+      {
+        imageHint: "liferaycloud/elasticsearch",
+        serviceId: "search",
+      },
+      {
+        imageHint: "liferaycloud/database",
+        serviceId: "database",
+      },
+    ],
+  },
+  {
+    createdAt: 1598375371394,
+    metadata: {
+      user: {
+        userEmail: "zeno.rocha@liferay.com",
+        initials: "Z",
+        name: "Zeno Rocha",
+        photoUrl:
+          "https://gravatar.com/avatar/7df4c367ea3a5475f05db9e2d32d5b13?d=blank&s=70",
+      },
+    },
+    buildId: "5",
+    projectId: "acme",
+    type: "BUILD_STARTED",
+    services: [
+      {
+        imageHint: "liferaycloud/backup",
+        serviceId: "backup",
+      },
+      {
+        imageHint: "liferaycloud/nginx",
+        serviceId: "webserver",
+      },
+      {
+        imageHint: "liferaycloud/liferay-dxp",
+        serviceId: "liferay",
+      },
+      {
+        imageHint: "liferaycloud/elasticsearch",
+        serviceId: "search",
+      },
+      {
+        imageHint: "liferaycloud/database",
+        serviceId: "database",
+      },
+    ],
+  },
+  {
+    createdAt: 1598375328031,
+    metadata: {
+      user: {
+        userEmail: "zeno.rocha@liferay.com",
+        initials: "Z",
+        name: "Zeno Rocha",
+        photoUrl:
+          "https://gravatar.com/avatar/7df4c367ea3a5475f05db9e2d32d5b13?d=blank&s=70",
+      },
+    },
+    buildId: "5",
+    projectId: "acme",
+    type: "CI_STARTED",
+  },
+];
 const ActivitiesGroup: React.FC = () => {
+  const getProcessingTime = (type: string) => {
+    return build.find((item) => item.type === type)?.createdAt;
+  };
+
   return (
     <Card style={{ width: 546 }} className="activities-group">
       <div>
@@ -13,10 +197,13 @@ const ActivitiesGroup: React.FC = () => {
       <div className="mt-6">
         <Timeline>
           <TimeLineHeader />
-          <TimeLineItem />
-          <TimeLineItem />
-          <TimeLineItem />
-          <TimeLineItem />
+          {build.map((item) => (
+            <TimeLineItem
+              item={item}
+              key={item.buildId + item.type}
+              getProcessingTime={getProcessingTime}
+            />
+          ))}
         </Timeline>
       </div>
     </Card>
